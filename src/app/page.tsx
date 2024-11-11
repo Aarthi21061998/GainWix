907dea7fde563c9a6569b977a9741807bbeae1ec
Aarthi-aptonworks <aarthi@aptonworks.com>
2024-11-11 14:40:31 +0530

    changrs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,35 @@
-'use client';
+"use client";
 
-import Image from 'next/image';
-import { MutableRefObject } from 'react';
+import Image from "next/image";
+import { MutableRefObject } from "react";
 
-import Video1 from '@icons/SegmentVideo.gif';
-// import Video from '@icons/Strategy.gif';
-import Video from "../assets/Icons/Strategy.gif"
-import Video2 from '@icons/final.gif';
+import Video1 from "../assets/../assets/Icons/SegmentVideo.gif";
+// import Video from 'icons/Strategy.gif';
+import Video from "../assets/Icons/Strategy.gif";
+// import Video2 from 'icons/final.gif';
 
-import Header from '@icons/header.gif';
+import Video2 from "../assets/Icons/final.gif";
 
-import MetaIcon from '@icons/meta.svg';
-import MainImage from '@icons/homeIcon.svg';
+import Header from "../assets/Icons/header.gif";
 
-// import LightBackGround from "@icons/light.svg";
-import HomePage from '@icons/HomePage.svg';
-import Marketing_first from '@icons/Strategy Animation.svg';
-import SegmentAnimation from '@icons/Segment Animation.svg';
-import Strategy from '@icons/Strategy.svg';
-import Segmentation from '@icons/Segmentation.svg';
-import Scale from '@icons/Scale.svg';
-import DigitalMarketing from '@components/DigitalMarketing';
-import Goals from '@components/Goals';
-import { useEffect, useRef } from 'react';
+import MetaIcon from "../assets/Icons/meta.svg";
+import MainImage from "../assets/Icons/homeIcon.svg";
 
-import gsap from 'gsap';
-import BrandScroll from '@components/BrandScroll';
-import Growth from '@components/Growth';
-import { MarketingGoalsList, MarketingGoalsListType } from '@constants/home';
+// import LightBackGround from "../assets/Icons/light.svg";
+import HomePage from "../assets/Icons/HomePage.svg";
+import Marketing_first from "../assets/Icons/Strategy Animation.svg";
+import SegmentAnimation from "../assets/Icons/Segment Animation.svg";
+import Strategy from "../assets/Icons/Strategy.svg";
+import Segmentation from "../assets/Icons/Segmentation.svg";
+import Scale from "../assets/Icons/Scale.svg";
+import DigitalMarketing from "@components/DigitalMarketing";
+import Goals from "@components/Goals";
+import { useEffect, useRef } from "react";
+
+import gsap from "gsap";
+import BrandScroll from "@components/BrandScroll";
+import Growth from "@components/Growth";
+import { MarketingGoalsList, MarketingGoalsListType } from "@constants/home";
 
 export default function Home() {
   const imageRef = useRef(null);
@@ -43,7 +45,7 @@ export default function Home() {
       yoyo: true,
       repeat: -1,
       duration: 3,
-      ease: 'power1.inOut',
+      ease: "power1.inOut",
     });
   }, []);
 
@@ -62,8 +64,8 @@ export default function Home() {
         duration: 1,
         scrollTrigger: {
           trigger: goalsRef.current,
-          start: 'top center',
-          end: 'bottom center',
+          start: "top center",
+          end: "bottom center",
           scrub: true,
           onLeave: () => {
             if (divRef.current) {
@@ -89,102 +91,102 @@ export default function Home() {
   }, []);
 
   return (
-    <div className='w-full h-full bg-[#060A22]'>
-      <div className='w-[100%] h-[30vh] flex justify-center text-[3vw] font-medium font-Sora py-10 '>
-        <h1 className='text-[3vw] font-medium text-[#FFFFFF]  font-Sora'>
-          <p className='pl-10'>Transform your digital marketing</p>
+    <div className="w-full h-full bg-[#060A22]">
+      <div className="w-[100%] h-[30vh] flex justify-center text-[3vw] font-medium font-Sora py-10 ">
+        <h1 className="text-[3vw] font-medium text-[#FFFFFF]  font-Sora">
+          <p className="pl-10">Transform your digital marketing</p>
           with awesome data-driven
-          <span className='from-[#8C76F7] via-[#8C76F7] to-[#F9AD68] bg-gradient-to-r bg-clip-text text-transparent font-Sora'>
-            {' '}
+          <span className="from-[#8C76F7] via-[#8C76F7] to-[#F9AD68] bg-gradient-to-r bg-clip-text text-transparent font-Sora">
+            {" "}
             Generative AI
           </span>
         </h1>
       </div>
-      <div className='w-[100%] flex justify-center text-[3vw] font-medium font-Sora'>
-        <h1 className='text-[1vw] font-medium text-[#D7DEDA] -mt-14 font-Inter'>
+      <div className="w-[100%] flex justify-center text-[3vw] font-medium font-Sora">
+        <h1 className="text-[1vw] font-medium text-[#D7DEDA] -mt-14 font-Inter">
           <p>
             Say goodbye to fragmented, ineffective marketing. Transforming your
             agency into a Cutting-edge, AI-powered
           </p>
-          <p className='pl-14'>
+          <p className="pl-14">
             powerhouse. Experience a seamless, data-driven approach that drives
             Real results.
           </p>
         </h1>
       </div>
-      <div className='w-[100%] flex justify-center text-[3vw] font-medium font-inter gap-5 my-5'>
-        <button className='btn btn_primary'>Request Demo</button>
+      <div className="w-[100%] flex justify-center text-[3vw] font-medium font-inter gap-5 my-5">
+        <button className="btn btn_primary">Request Demo</button>
         <button>
-          <div className='w-[100%] flex flex-row border border-[#111449] p-4 rounded-full align-center justify-center'>
-            <Image src={MetaIcon} alt='MetaIcon' className='w-10 h-4' />
-            <p className='text-[1.05vw] text-[#FFFFFF] font-Inter'>
+          <div className="w-[100%] flex flex-row border border-[#111449] p-4 rounded-full align-center justify-center">
+            <Image src={MetaIcon} alt="MetaIcon" className="w-10 h-4" />
+            <p className="text-[1.05vw] text-[#FFFFFF] font-Inter">
               Tech Partner
             </p>
           </div>
         </button>
       </div>
       <div
-        className='relative'
+        className="relative"
         style={{
-          width: '100%',
-          height: 'auto',
+          width: "100%",
+          height: "auto",
         }}
       >
-        <Image src={HomePage} alt='' className='' />{' '}
+        <Image src={HomePage} alt="" className="" />{" "}
       </div>
       <Image
         src={MainImage}
-        alt='home'
-        className='absolute top-[20vw]'
+        alt="home"
+        className="absolute top-[20vw]"
         ref={imageRef}
         style={{
-          width: '100%',
-          height: 'auto',
-          objectFit: 'cover',
+          width: "100%",
+          height: "auto",
+          objectFit: "cover",
         }}
       />
       <div
         ref={divRef}
-        className='overflow-hidden absolute top-[49vw] left-[34vw] w-[32vw] h-[44vh]  z-50'
+        className="overflow-hidden absolute top-[49vw] left-[34vw] w-[32vw] h-[44vh]  z-50"
       >
-        <Image src={Header} alt='' />
-      </div>{' '}
-      <div className='py-10' ref={goalsRef}>
+        <Image src={Header} alt="" />
+      </div>{" "}
+      <div className="py-10" ref={goalsRef}>
         {/* <div className="py-10"> */}
         <Goals />
       </div>
-      <div className='relative'>
-        <Image src={Strategy} alt='Background' className='w-full h-auto' />
-        <div className='flex flex-col items-center justify-center'>
-          <div className='flex justify-center text-[3vw] font-medium font-Sora absolute top-4'>
-            <h1 className='text-[3vw] font-medium font-Sora text-[#FFFFFF] pt-16'>
-              <p className='pl-10'>Integrate the &quot;3 S&quot; AI in your </p>
-              <span className='from-[#8C76F7] via-[#8C76F7] to-[#F9AD68] bg-gradient-to-r bg-clip-text text-transparent pl-32'>
+      <div className="relative">
+        <Image src={Strategy} alt="Background" className="w-full h-auto" />
+        <div className="flex flex-col items-center justify-center">
+          <div className="flex justify-center text-[3vw] font-medium font-Sora absolute top-4">
+            <h1 className="text-[3vw] font-medium font-Sora text-[#FFFFFF] pt-16">
+              <p className="pl-10">Integrate the &quot;3 S&quot; AI in your </p>
+              <span className="from-[#8C76F7] via-[#8C76F7] to-[#F9AD68] bg-gradient-to-r bg-clip-text text-transparent pl-32">
                 digital marketing
               </span>
             </h1>
           </div>
         </div>
 
-        <div className='w-[100%]  flex flex-row  px-10 absolute top-44'>
+        <div className="w-[100%]  flex flex-row  px-10 absolute top-44">
           <div
-            className='w-[45%] flex justify-center relative '
+            className="w-[45%] flex justify-center relative "
             ref={imageContainerRef}
           >
             <Image
               src={Marketing_first}
-              alt='marketing1'
-              className='w-full h-auto blend-screen'
+              alt="marketing1"
+              className="w-full h-auto blend-screen"
             />
-            <div className='absolute  flex justify-center items-center'>
-              <Image src={Video} alt='' className='w-[30vw] h-[55vh]' />
+            <div className="absolute  flex justify-center items-center">
+              <Image src={Video} alt="" className="w-[30vw] h-[55vh]" />
             </div>
           </div>
-          <div className='w-[50%] flex flex-col justify-center  text-[white]'>
-            <h1 className='text-[2vw] font-semibold pb-5 font-Sora pl-20'>
+          <div className="w-[50%] flex flex-col justify-center  text-[white]">
+            <h1 className="text-[2vw] font-semibold pb-5 font-Sora pl-20">
               Strategy AI
             </h1>
-            <p className='w-[70%] pl-20 font-Inter font-medium'>
+            <p className="w-[70%] pl-20 font-Inter font-medium">
               Develop smarter, data-driven strategies that adapt to market
               trends and customer behavior, ensuring your campaigns are always
               on target
@@ -192,49 +194,49 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className='relative flex flex-row'>
-        <Image src={Segmentation} alt='' />
+      <div className="relative flex flex-row">
+        <Image src={Segmentation} alt="" />
 
-        <div className='w-[100%] flex flex-row px-40 absolute top-10'>
-          <div className='w-[45%] flex flex-col justify-center text-[white]'>
-            <h1 className='text-[2vw] font-Sora font-bold px-20 pb-5'>
+        <div className="w-[100%] flex flex-row px-40 absolute top-10">
+          <div className="w-[45%] flex flex-col justify-center text-[white]">
+            <h1 className="text-[2vw] font-Sora font-bold px-20 pb-5">
               Segmentation AI
             </h1>
-            <p className=' text-start pl-20 font-Inter font-semibold'>
+            <p className=" text-start pl-20 font-Inter font-semibold">
               Accurately segment your audience using AI to deliver personalized
               messages that resonate, driving higher engagement and conversion
               rates.
             </p>
           </div>
-          <div className='w-[55%] flex justify-center'>
+          <div className="w-[55%] flex justify-center">
             <Image
               src={SegmentAnimation}
-              alt='marketing1'
-              className='   blend-screen'
+              alt="marketing1"
+              className="   blend-screen"
             />
           </div>
-          <div className='  rounded-full'>
+          <div className="  rounded-full">
             <Image
               src={Video1}
-              alt=''
-              className='absolute right-[17.3vw] top-[3vw]  w-[30vw] h-[55vh]'
+              alt=""
+              className="absolute right-[17.3vw] top-[3vw]  w-[30vw] h-[55vh]"
             />
           </div>
         </div>
       </div>
-      <div className='relative flex flex-row'>
-        <Image src={Scale} alt='' />
+      <div className="relative flex flex-row">
+        <Image src={Scale} alt="" />
 
-        <div className='w-[100%] flex flex-row px-40 absolute top-10'>
-          <div className='w-[45%] flex justify-center'>
-            {' '}
-            <Image src={Video2} alt='' />
+        <div className="w-[100%] flex flex-row px-40 absolute top-10">
+          <div className="w-[45%] flex justify-center">
+            {" "}
+            <Image src={Video2} alt="" />
           </div>
-          <div className='w-[50%] flex flex-col justify-center text-[white]'>
-            <h1 className='text-[2vw] font-Sora font-bold px-20 pb-5'>
+          <div className="w-[50%] flex flex-col justify-center text-[white]">
+            <h1 className="text-[2vw] font-Sora font-bold px-20 pb-5">
               Scale Through Automation
             </h1>
-            <p className='text-start font-Sora font-semibold pl-20'>
+            <p className="text-start font-Sora font-semibold pl-20">
               Effortlessly scale your marketing efforts with Automation,
               optimizing performance and expanding your reach without
               compromising quality or efficiency.
@@ -244,34 +246,34 @@ export default function Home() {
       </div>
       {/* Slider Section */}
       <DigitalMarketing />
-      <div className='py-10'>
+      <div className="py-10">
         <BrandScroll />
       </div>
-      <div className='flex flex-col items-center justify-center'>
-        <div className='flex justify-center text-[3vw] font-medium font-Sora '>
-          <h1 className='text-[3vw] font-Sora font-medium text-[#FFFFFF] py-16'>
-            <p className='pl-10 font-Sora'>
-              The 3 S AI stabilizes your marketing goal{' '}
-            </p>{' '}
-            <span className='from-[#8C76F7] via-[#8C76F7] to-[#F9AD68] bg-gradient-to-r bg-clip-text text-transparent pl-36'>
-              And increase performance all time.{' '}
+      <div className="flex flex-col items-center justify-center">
+        <div className="flex justify-center text-[3vw] font-medium font-Sora ">
+          <h1 className="text-[3vw] font-Sora font-medium text-[#FFFFFF] py-16">
+            <p className="pl-10 font-Sora">
+              The 3 S AI stabilizes your marketing goal{" "}
+            </p>{" "}
+            <span className="from-[#8C76F7] via-[#8C76F7] to-[#F9AD68] bg-gradient-to-r bg-clip-text text-transparent pl-36">
+              And increase performance all time.{" "}
             </span>
           </h1>
         </div>
 
-        <div className='w-full flex flex-row gap-10 justify-center'>
+        <div className="w-full flex flex-row gap-10 justify-center">
           {MarketingGoalsList?.map((data: MarketingGoalsListType) => (
             <div
-              className='w-[25%] h-[45vh] rounded-lg from-[#0A0D2A] via-[#0A0D2A] to-[#2D3154] bg-gradient-to-r '
+              className="w-[25%] h-[45vh] rounded-lg from-[#0A0D2A] via-[#0A0D2A] to-[#2D3154] bg-gradient-to-r "
               key={data?.id}
             >
-              <div className='flex-col text-white w-full flex items-center justify-center text-[1.2vw] mt-[10vw]'>
-                <p className='font-Sora font-semibold'>{data?.content}</p>
-                <span className=' font-Sora font-semibold'>
+              <div className="flex-col text-white w-full flex items-center justify-center text-[1.2vw] mt-[10vw]">
+                <p className="font-Sora font-semibold">{data?.content}</p>
+                <span className=" font-Sora font-semibold">
                   {data?.content1}
                 </span>
 
-                <p className='w-auto min-w-10 text-[0.95vw] px-10 py-5 flex justify-center  font-Sora items-center text-center font-medium '>
+                <p className="w-auto min-w-10 text-[0.95vw] px-10 py-5 flex justify-center  font-Sora items-center text-center font-medium ">
                   {data?.details}
                 </p>
               </div>

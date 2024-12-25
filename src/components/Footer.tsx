eac874f36703ee9ab4574629d4a9b703a8db1677
Aarthi-aptonworks <aarthi@aptonworks.com>
2024-12-25 10:25:30 +0530

    productpage

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,122 +1,3 @@
-// "use client";
-
-// import { FOOTER_LINKS } from "@constants/footer";
-// import Image from "next/image";
-// import Link from "next/link";
-// import FullLogo from "@logos/full-logo.svg";
-// import Facebook from "@custom-svg/Facebook";
-// import Instagram from "@custom-svg/Instagram";
-// import Linkedin from "@custom-svg/Linkedin";
-// import Twitter from "@custom-svg/Twitter";
-// import { useState } from "react";
-// import Arrow from "../assets/Icons/Dropdown.svg";
-
-// export default function Footer() {
-//   const [openSections, setOpenSections] = useState({});
-
-//   type OpenSections = {
-//     [key: string]: boolean;
-//   };
-
-//   const toggleSection = (id: string) => {
-//     setOpenSections((prevState: OpenSections) => ({
-//       ...prevState,
-//       [id]: !prevState[id],
-//     }));
-//   };
-
-//   return (
-//     <div className="xs:px-5 md:layout text-[white] bg-[#060A22]">
-//       <div className="xl:min-h-[50vh] pt-[4.5vw] pl-[3vw] lg:flex pb-[1vw] gap-20 xs:block">
-//         <div className="w-[40vw] lg:block">
-//           <Image
-//             src={FullLogo}
-//             alt="logo"
-//             className="lg:w-[13.8vw] lg:h-[3.2vw] mb-6 xs:w-[50vw] xs:h-[5vh]"
-//           />
-
-//           <p className="lg:max-w-[50vw] lg:w-full flex items-center gap-1 font-medium cursor-pointer font-Inter text-[#908EB5] xs:text-[14px]  xs:w-[88vw] xs:mb-5 ">
-//             Transform your digital marketing <br />
-//             with powerful, data-driven Generative AI
-//           </p>
-//           <div className=" lg:flex flex-row items-center gap-5 mt-10 xs:hidden ">
-//             <div className="w-10 h-10 rounded-2xl bg-[#2B2E4F] flex justify-center items-center hover:bg-[#908EB5] hover-instagram">
-//               <Instagram />
-//             </div>
-//             <div className="w-10 h-10 rounded-2xl bg-[#2B2E4F] flex justify-center items-center hover:bg-[#908EB5] hover-instagram">
-//               <Facebook />
-//             </div>
-//             <div className="w-10 h-10 rounded-2xl bg-[#2B2E4F] flex justify-center items-center hover:bg-[#908EB5] hover-instagram">
-//               <Linkedin />
-//             </div>
-//             <div className="w-10 h-10 rounded-2xl bg-[#2B2E4F] flex justify-center items-center hover:bg-[#908EB5] hover-instagram">
-//               <Twitter />
-//             </div>
-//           </div>
-//         </div>
-
-//         <div className="xs:w-[50vw] lg:flex justify-between xs:block">
-//           {FOOTER_LINKS?.map((l) => {
-//             const isOpen = openSections[l.id];
-
-//             return (
-//               <div key={l.id} className={`flex flex-col gap-5 xs:mt-3 `}>
-//                 <h4
-//                   className="lg:text-[0.97vw] xs:text-[2vw] text-[#908EB5] font-Inter font-bold flex xs:justify-between  xs:w-[80vw] lg:w-[10vw] xs:py-2 items-center lg:justify-start cursor-pointer lg:cursor-default"
-//                   onClick={() => toggleSection(l.id)}
-//                 >
-//                   {l.label}
-//                   <div className="lg:hidden ml-2">
-//                     <Image
-//                       src={Arrow}
-//                       alt="arrow"
-//                       className={`transition-transform duration-300 xs:w-[6vw] h-[3vh]${
-//                         isOpen ? "rotate-180" : "rotate-0"
-//                       }`}
-//                     />
-//                   </div>
-//                 </h4>
-
-//                 <ul
-//                   className={`flex flex-col  ${
-//                     isOpen || window.innerWidth >= 992 ? "block" : "hidden"
-//                   } lg:block`}
-//                 >
-//                   {l.lists?.map((list) => (
-//                     <div
-//                       key={list?.id}
-//                       className="w-full xs:text-[4vw] md:text-[2vw] lg:text-[0.87vw] py-2 font-medium text-[#55567A] font-Inter hover:text-[#e2deff]"
-//                     >
-//                       {list.label}
-//                     </div>
-//                   ))}
-//                 </ul>
-//               </div>
-//             );
-//           })}
-//         </div>
-//       </div>
-
-//       <div className="w-full lg:h-20 flex-center xs:flex-col  xs:h-40 gradient-border font-Inter font-medium text-[#908EB5] xs:text-[3.5vw]  md:text-[2vw] lg:text-[1vw]">
-//         &copy; GainWix 2024, All Rights Reserved.
-//         <div className="xs:flex  lg:hidden items-center gap-5 mt-10 xs:mb-5  xs:justify-center">
-//           <div className=" xs:rounded-[50%]   xs:w-[12vw] xs:h-[5vh] md:w-[7vw] md:h-[5vh] md:rounded-[50%] lg:size-10 lg:rounded-2xl bg-[#2B2E4F] flex-center hover:bg-[#908EB5] hover-instagram">
-//             <Instagram />
-//           </div>
-//           <div className=" xs:rounded-[50%]   xs:w-[12vw] xs:h-[5vh] md:w-[7vw] md:h-[5vh] md:rounded-[50%] lg:size-10 lg:rounded-2xl bg-[#2B2E4F] flex-center hover:bg-[#908EB5] hover-instagram">
-//             <Facebook />
-//           </div>
-//           <div className="xs:rounded-[50%]   xs:w-[12vw] xs:h-[5vh] md:w-[7vw] md:h-[5vh] md:rounded-[50%] lg:size-10 lg:rounded-2xl bg-[#2B2E4F] flex-center hover:bg-[#908EB5] hover-instagram">
-//             <Linkedin />
-//           </div>
-//           <div className="xs:rounded-[50%]   xs:w-[12vw] xs:h-[5vh] md:w-[7vw] md:h-[5vh] md:rounded-[50%] lg:size-10 lg:rounded-2xl bg-[#2B2E4F] flex-center hover:bg-[#908EB5] hover-instagram">
-//             <Twitter />
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
 "use client";
 
 import { FOOTER_LINKS } from "@constants/footer";
@@ -130,23 +11,25 @@ import Twitter from "@custom-svg/Twitter";
 import { useState } from "react";
 import Arrow from "../assets/Icons/Dropdown.svg";
 
+type ListItem = {
+  id: number;
+  label: string;
+  path?: string; // Path is optional
+  href?: string; // Href is optional
+};
+
 type FooterLink = {
   id: number;
   label: string;
-  lists: Array<
-    | { id: number; label: string; path: string }
-    | { id: number; label: string; href: string }
-  >;
+  lists: ListItem[];
 };
 
-type OpenSections = {
-  [key: string]: boolean;
-};
+type OpenSections = Record<string, boolean>;
 
-export default function Footer() {
+export default function Footer(): JSX.Element {
   const [openSections, setOpenSections] = useState<OpenSections>({});
 
-  const toggleSection = (id: string) => {
+  const toggleSection = (id: string): void => {
     setOpenSections((prevState) => ({
       ...prevState,
       [id]: !prevState[id],
@@ -156,65 +39,55 @@ export default function Footer() {
   return (
     <div className="xs:px-5 md:layout text-white bg-[#060A22]">
       <div className="xl:min-h-[50vh] pt-[4.5vw] md:pl-[3vw] lg:flex pb-[1vw] gap-20 xs:block xl:w-[80vw] 2xl:w-[64vw] xl:m-auto">
+        {/* Logo and Description */}
         <div className="w-[40vw] lg:block">
           <Image
             src={FullLogo}
-            alt="logo"
+            alt="GainWix Logo"
             className="lg:w-[13.8vw] lg:h-[3.2vw] mb-6 xs:w-[50vw] xs:h-[5vh]"
           />
-
-          <p className="lg:max-w-[50vw] lg:w-full flex items-center gap-1 font-medium cursor-pointer font-Inter text-[#908EB5] xs:text-[16px] md:text-[18px]  xs:w-[88vw] xs:mb-5 ">
+          <p className="lg:max-w-[50vw] lg:w-full flex items-center gap-1 font-medium cursor-pointer font-Inter text-[#908EB5] xs:text-[16px] md:text-[18px] xs:w-[88vw] xs:mb-5">
             Transform your digital marketing <br />
             with powerful, data-driven Generative AI
           </p>
-
+          {/* Social Media Links */}
           <div className="lg:flex flex-row items-center gap-5 mt-10 xs:hidden">
-            <div className="w-10 h-10 rounded-2xl bg-[#2B2E4F] flex justify-center items-center hover:bg-[#908EB5]  hover-instagram ">
-              <a href="https://www.instagram.com/gainwix/" target="_blank">
-                {" "}
-                <Instagram />{" "}
-              </a>
-            </div>
-            <div className="w-10 h-10 rounded-2xl bg-[#2B2E4F] flex justify-center items-center hover:bg-[#908EB5]  hover-instagram">
-              <a
-                href="https://www.facebook.com/people/GainWix/61556584534406/"
-                target="_blank"
-              >
-                <Facebook />
-              </a>
-            </div>
-            <div className="w-10 h-10 rounded-2xl bg-[#2B2E4F] flex justify-center items-center hover:bg-[#908EB5]  hover-instagram">
-              <a href="https://www.linkedin.com/company/gainwixai/">
-                <Linkedin />
-              </a>
-            </div>
-            <div className="w-10 h-10 rounded-2xl bg-[#2B2E4F] flex justify-center items-center hover:bg-[#908EB5]  hover-instagram">
-              <a
-                href="https://x.com/i/flow/login?redirect_after_login=%2Fgainwixai%2F"
-                target="_blank"
-              >
-                <Twitter />
-              </a>
-            </div>
+            <SocialMediaLink
+              href="https://www.instagram.com/gainwix/"
+              Icon={Instagram}
+            />
+            <SocialMediaLink
+              href="https://www.facebook.com/people/GainWix/61556584534406/"
+              Icon={Facebook}
+            />
+            <SocialMediaLink
+              href="https://www.linkedin.com/company/gainwixai/"
+              Icon={Linkedin}
+            />
+            <SocialMediaLink
+              href="https://x.com/i/flow/login?redirect_after_login=%2Fgainwixai%2F"
+              Icon={Twitter}
+            />
           </div>
         </div>
 
-        <div className="xs:w-[50vw] lg:flex justify-between xs:block pointer ">
+        {/* Footer Links */}
+        <div className="xs:w-[50vw] lg:flex justify-between xs:block pointer">
           {FOOTER_LINKS.length > 0 &&
-            FOOTER_LINKS.map((data: FooterLink, index: number) => {
-              const isOpen = openSections[data.id.toString()]; // Ensure id is a string for openSections
+            FOOTER_LINKS.map((data: FooterLink) => {
+              const isOpen = openSections[data.id.toString()];
 
               return (
                 <div key={data.id} className="flex flex-col gap-5 xs:mt-3">
                   <h4
                     className="lg:text-[0.97vw] xs:text-[3.5vw] text-[#908EB5] font-Inter font-bold flex xs:justify-between xs:w-[80vw] lg:w-[10vw] xs:py-2 items-center lg:justify-start cursor-pointer lg:cursor-default"
-                    onClick={() => toggleSection(data.id.toString())} // Convert id to string if needed
+                    onClick={() => toggleSection(data.id.toString())}
                   >
                     {data.label}
                     <div className="lg:hidden ml-2">
                       <Image
                         src={Arrow}
-                        alt="arrow"
+                        alt="Dropdown Arrow"
                         className={`transition-transform duration-300 xs:w-[6vw] h-[3vh] ${
                           isOpen ? "rotate-180" : "rotate-0"
                         }`}
@@ -232,14 +105,14 @@ export default function Footer() {
                     } lg:block`}
                   >
                     {data.lists.map((list) => (
-                      <div
+                      <li
                         key={list.id}
-                        className="w-full xs:text-[4vw] md:text-[2vw] lg:text-[0.87vw] py-2 font-medium text-[#55567A] font-Inter hover:text-[#e2deff] pointer "
+                        className="w-full xs:text-[4vw] md:text-[2vw] lg:text-[0.87vw] py-2 font-medium text-[#55567A] font-Inter hover:text-[#e2deff] pointer"
                       >
                         <Link href={list.path ?? list.href ?? "#"}>
                           {list.label}
                         </Link>
-                      </div>
+                      </li>
                     ))}
                   </ul>
                 </div>
@@ -248,23 +121,35 @@ export default function Footer() {
         </div>
       </div>
 
+      {/* Footer Bottom Section */}
       <div className="w-full lg:h-20 flex-center xs:flex-col xs:h-40 gradient-border font-Inter font-medium text-[#908EB5] xs:text-[3.5vw] md:text-[2vw] lg:text-[1vw]">
         &copy; GainWix 2024, All Rights Reserved.
+        {/* Mobile Social Media Links */}
         <div className="xs:flex lg:hidden items-center gap-5 mt-10 xs:mb-5 xs:justify-center">
-          <div className="xs:rounded-[50%] xs:w-[11.5vw] xs:h-[5.5vh] md:w-[7vw] md:h-[5vh] md:rounded-[50%] bg-[#2B2E4F] flex-center hover:bg-[#908EB5]">
-            <Instagram />
-          </div>
-          <div className="xs:rounded-[50%] xs:w-[11.5vw] xs:h-[5.5vh] md:w-[7vw] md:h-[5vh] bg-[#2B2E4F] flex-center hover:bg-[#908EB5]">
-            <Facebook />
-          </div>
-          <div className="xs:rounded-[50%] xs:w-[11.5vw] xs:h-[5.5vh] md:w-[7vw] md:h-[5vh] bg-[#2B2E4F] flex-center hover:bg-[#908EB5]">
-            <Linkedin />
-          </div>
-          <div className="xs:rounded-[50%] xs:w-[11.5vw] xs:h-[6vh] md:w-[7vw] md:h-[5vh] bg-[#2B2E4F] flex-center hover:bg-[#908EB5]">
-            <Twitter />
-          </div>
+          <SocialMediaLink Icon={Instagram} />
+          <SocialMediaLink Icon={Facebook} />
+          <SocialMediaLink Icon={Linkedin} />
+          <SocialMediaLink Icon={Twitter} />
         </div>
       </div>
     </div>
   );
 }
+
+// Social Media Link Component
+type SocialMediaLinkProps = {
+  href?: string;
+  Icon: React.ComponentType;
+};
+
+const SocialMediaLink: React.FC<SocialMediaLinkProps> = ({ href, Icon }) => (
+  <div className="w-10 h-10 rounded-2xl bg-[#2B2E4F] flex justify-center items-center hover:bg-[#908EB5]">
+    {href ? (
+      <a href={href} target="_blank" rel="noopener noreferrer">
+        <Icon />
+      </a>
+    ) : (
+      <Icon />
+    )}
+  </div>
+);
